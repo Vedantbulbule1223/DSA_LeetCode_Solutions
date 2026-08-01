eee2a4fac17c1198eac0871e6d2fb287b9c02888
Vedantbulbule1223 <169058995+Vedantbulbule1223@users.noreply.github.com>
2026-08-01 09:15:27 +0530

    Time: 0 ms (100%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-
-    int solve(vector<int>& nums, int i, int j, vector<vector<int>>& dp) {
-
-        if (i == j)
-            return nums[i];
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int left = nums[i] - solve(nums, i + 1, j, dp);
-
-        int right = nums[j] - solve(nums, i, j - 1, dp);
-
-        return dp[i][j] = max(left, right);
-    }
-
     bool predictTheWinner(vector<int>& nums) {
 
         int n = nums.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        return solve(nums, 0, n - 1, dp) >= 0;
+        for (int i = 0; i < n; i++)
+            dp[i][i] = nums[i];
+
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i + len - 1 < n; i++) {
+
+                int j = i + len - 1;
+
+                int left = nums[i] - dp[i + 1][j];
+
+                int right = nums[j] - dp[i][j - 1];
+
+                dp[i][j] = max(left, right);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
     }
 };
